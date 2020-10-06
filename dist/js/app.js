f40cba24a64ea36c833d129dbf9058d2290484d4
Mirko <mirko.longo.1987@gmail.com>
2020-10-06 19:14:08 +0200

    button e chiamata funzionanti

--- dist/js/app.js
+++ dist/js/app.js
@@ -93,17 +93,51 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-var content = document.getElementById('content');
-var content = document.getElementById('show-more');
-var button = document.getElementById('button');
+var button = document.getElementById('button'); // var dataSku = document.querySelector('.content').getAttribute('data-sku');
+//
+// var dataSkuOb = { 0: dataSku}
+// console.log(dataSkuOb);
+// document.querySelector('.content').addEventListener("click", dataSkuOb);
 
-button.onclick = function () {
-  fetch('https://jsonplaceholder.typicode.com/posts/1').then(function (response) {
+document.getElementById('button').addEventListener("click", function () {
+  var dataSku = document.querySelector('.content').getAttribute('data-sku');
+  console.log(dataSku);
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSku).then(function (response) {
     return response.json();
   }).then(function (data) {
-    return console.log(data);
+    return console.log(data.title);
   });
-};
+});
+document.getElementById('button-due').addEventListener("click", function () {
+  var dataSkuDue = document.querySelector('.content-due').getAttribute('data-sku');
+  console.log(dataSkuDue);
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSkuDue).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    return console.log(data.title);
+  });
+});
+document.getElementById('button-tre').addEventListener("click", function () {
+  var dataSkuTre = document.querySelector('.content-tre').getAttribute('data-sku');
+  console.log(dataSkuTre);
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSkuTre).then(function (response) {
+    return response.json();
+  }).then(function (data) {
+    return console.log(data.title);
+  });
+}); // button.onclick = function() {
+//
+//
+//   var dataSku = document.querySelector('.content').getAttribute('data-sku');
+//   console.log(self.dataSku); //
+//
+//   fetch('https://jsonplaceholder.typicode.com/posts/' + dataSku)
+//   .then(response => response.json())
+//   .then(data => console.log(data.title)
+//
+//   )
+//
+// }
 
 /***/ }),
 
--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,68 @@
 
-var content = document.getElementById('content');
-
-var content = document.getElementById('show-more');
-
 var button = document.getElementById('button');
+// var dataSku = document.querySelector('.content').getAttribute('data-sku');
+//
+// var dataSkuOb = { 0: dataSku}
+// console.log(dataSkuOb);
 
-button.onclick = function() {
 
-  fetch('https://jsonplaceholder.typicode.com/posts/1')
+// document.querySelector('.content').addEventListener("click", dataSkuOb);
+
+document.getElementById('button').addEventListener("click", function(){
+
+  var dataSku = document.querySelector('.content').getAttribute('data-sku');
+  console.log(dataSku);
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSku)
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => console.log(data.title)
 
-}
+  )
+
+
+});
+
+document.getElementById('button-due').addEventListener("click", function(){
+
+  var dataSkuDue = document.querySelector('.content-due').getAttribute('data-sku');
+  console.log(dataSkuDue);
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSkuDue)
+  .then(response => response.json())
+  .then(data => console.log(data.title)
+
+  )
+
+
+});
+
+document.getElementById('button-tre').addEventListener("click", function(){
+
+  var dataSkuTre = document.querySelector('.content-tre').getAttribute('data-sku');
+  console.log(dataSkuTre);
+
+
+  fetch('https://jsonplaceholder.typicode.com/posts/' + dataSkuTre)
+  .then(response => response.json())
+  .then(data => console.log(data.title)
+
+  )
+
+
+});
+
+// button.onclick = function() {
+//
+//
+//   var dataSku = document.querySelector('.content').getAttribute('data-sku');
+//   console.log(self.dataSku); //
+//
+//   fetch('https://jsonplaceholder.typicode.com/posts/' + dataSku)
+//   .then(response => response.json())
+//   .then(data => console.log(data.title)
+//
+//   )
+//
+// }
